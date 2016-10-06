c5b4557f5306d6038766b9c3f69089136631ffc5
tgaribal <tgaribal@gmail.com>
2016-10-05 19:58:18 -0700

    made a lot of progress

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require ('../helpers/archive-helpers');
+
+var urlList = [];
+
+archive.readListOfUrls(function(urls) {
+  urls.forEach(url => {
+    if (url !== '') {
+      urlList.push(url);
+    }
+  });
+  archive.downloadUrls(urlList);
+});
